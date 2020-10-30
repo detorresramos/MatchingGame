91a571ba49bfd223ebb8a17f4e3c35707b1a110b
detorresramos <detorresramos1@gmail.com>
2020-10-30 09:00:39 -0500

    Initial Commit

---
+++ MatchingGame/MatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingGameApp.swift
+//  MatchingGame
+//
+//  Created by David Torres on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct MatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
